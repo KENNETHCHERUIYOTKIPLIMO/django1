5121a0fc1fa45a4991ed4c7704dce8a60b8ecd98
Ken <cheruiyotk134@gmail.com>
2023-10-23 23:07:07 +0300

    django project

---
+++ Teachers/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .import views
+
+urlpatterns = [
+
+    path('', views.welcome),
+
+    path('about', views.about)
+
+]
---
+++ Teachers/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+
+def welcome(request):
+    return HttpResponse("Welcome to emobilis")
+
+
+def about(request):
+    return HttpResponse("About emobilis")
